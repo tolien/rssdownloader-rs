205f7a53fe5bc3cac5ba6740471628c9e6109083
tolien <tolien@users.noreply.github.com>
2020-03-15 17:47:34 +0000

    Take the logging code from currentcost-rs, but leave the log to file part commented out

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,7 @@ pub struct Config {
     pub global_download_dir: PathBuf,
     pub refresh_interval: Duration,
     pub feeds: Vec<FeedConfig>,
+    pub log_file_path: Option<PathBuf>,
 }
 
 impl Config {
@@ -147,6 +148,7 @@ impl Config {
             global_download_dir: PathBuf::from(download_dir),
             refresh_interval: sleep_interval,
             feeds: feed_objects,
+            log_file_path: None,
         })
     }
 }
