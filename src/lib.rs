a1aaed1d8146b08cda648f83b9917b9b53439336
Stuart Swindells <tolien@users.noreply.github.com>
2020-08-01 18:08:12 +0100

    Use match to handle if the config path isn't passed through to the Config constructor

--- src/lib.rs
+++ src/lib.rs
@@ -84,11 +84,13 @@ pub struct Config {
 
 impl Config {
     pub fn new(path_to_config: Option<PathBuf>) -> Result<Self, &'static str> {
-        let working_dir = dirs::home_dir().unwrap().join(".rssdownloader-rs");
-        let mut config_path = working_dir.join("config.toml");
-        if path_to_config.is_some() {
-            config_path = path_to_config.unwrap();
-        }
+        let config_path = match path_to_config {
+            Some(path) => path,
+            _ => {
+                let working_dir = dirs::home_dir().unwrap().join(".rssdownloader-rs");
+                working_dir.join("config.toml")
+            }
+        };
 
         debug!("Using config path {:?}", config_path);
         if let Ok(properties) = fs::read_to_string(config_path) {
