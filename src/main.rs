45d7572331053379ec873ec0c0bc76add8bb9b21
Stuart Swindells <tolien@users.noreply.github.com>
2020-08-07 08:05:15 +0000

    Squash merge branch logging This switches from fern to log4rs with some changes to log colouring Also includes a fix for issue #6 - -c/--config option will specify the location of the config file to use

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ extern crate chrono;
 #[macro_use]
 extern crate log;
 
-extern crate fern;
-
-use fern::colors::{Color, ColoredLevelConfig};
 extern crate dirs;
 
 use reqwest::blocking::Client;
@@ -15,12 +12,43 @@ use std::path::{Path, PathBuf};
 use std::thread;
 use std::time::Duration;
 
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Logger, Root};
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::filter::threshold::ThresholdFilter;
+use log4rs::Handle;
+
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    let config_result = Config::new();
+    let matches = App::new("rssdownloader-rs")
+        .arg(
+            Arg::with_name("config")
+                .short("-c")
+                .long("config")
+                .help("Path to the config file to use")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    let logger_handle = bootstrap_logger().unwrap();
+
+    let config_path;
+    if let Some(config_path_str) = matches.value_of("config") {
+        config_path = Some(PathBuf::from(config_path_str));
+    } else {
+        config_path = None;
+    }
+
+    let config_result = Config::new(config_path);
     if let Ok(config) = config_result {
-        let logger_result = setup_logger(&config);
+        let logger_result = apply_config_to_logger(&logger_handle, &config);
         if logger_result.is_err() {
-            panic!("Error applying fern logger");
+            panic!("Couldn't set up logger");
         }
 
         debug!(
@@ -38,7 +66,7 @@ fn main() {
 
         loop {
             for feed in &config.feeds {
-              handle_feed(feed, &client, &config);
+                handle_feed(feed, &client, &config);
             }
 
             let sleep_time = config.refresh_interval;
@@ -51,58 +79,56 @@ fn main() {
 }
 
 fn handle_feed(feed: &FeedConfig, client: &Client, config: &Config) {
-  let mut saved_state = SavedState::new().unwrap();
-  info!("Fetching {}", feed.name);
-  let rss_result = fetch_rss(&feed.url, &client);
-  if rss_result.is_err() {
-      error!("Failed to load RSS feed");
-      return;
-  }
-  let rss_channel = rss_result.unwrap();
-  for item in rss_channel.into_items() {
-      let title_result = item.title();
-      if title_result.is_none() {
-          debug!("No title found");
-          return;
-      }
-      let title = title_result.unwrap();
-      trace!("Title: {}", title);
-      if let Some(global_regex) = &feed.global_include_filter {
-          if !global_regex.is_match(title) {
-              return;
-          }
-      }
-      if let Some(global_exclude_regex) = &feed.global_exclude_filter {
-          if global_exclude_regex.is_match(title) {
-              return;
-          }
-      }
-      if feed.download_filter.is_match(title) {
-          let item_url = item.link().unwrap();
+    let mut saved_state = SavedState::new().unwrap();
+    info!("Fetching {}", feed.name);
+    let rss_result = fetch_rss(&feed.url, &client);
+    if rss_result.is_err() {
+        error!("Failed to load RSS feed");
+        return;
+    }
+    let rss_channel = rss_result.unwrap();
+    for item in rss_channel.into_items() {
+        let title_result = item.title();
+        if title_result.is_none() {
+            debug!("No title found");
+            return;
+        }
+        let title = title_result.unwrap();
+        trace!("Title: {}", title);
+        if let Some(global_regex) = &feed.global_include_filter {
+            if !global_regex.is_match(title) {
+                return;
+            }
+        }
+        if let Some(global_exclude_regex) = &feed.global_exclude_filter {
+            if global_exclude_regex.is_match(title) {
+                return;
+            }
+        }
+        if feed.download_filter.is_match(title) {
+            let item_url = item.link().unwrap();
 
-          let fetched_item = FetchedItem {
-              name: String::from(title),
-              url: String::from(item_url),
-          };
+            let fetched_item = FetchedItem {
+                name: String::from(title),
+                url: String::from(item_url),
+            };
 
-          if saved_state.fetched_before(&fetched_item).unwrap() {
-              debug!("Skipping previously fetched item {}", title);
-              return;
-          }
+            if saved_state.fetched_before(&fetched_item).unwrap() {
+                debug!("Skipping previously fetched item {}", title);
+                return;
+            }
 
-          info!("Matched title: {:?}", title);
-          debug!("url: {:?}", item_url);
-          let fetch_result =
-              fetch_item(item_url, &client, &config.global_download_dir);
-          if fetch_result.is_ok() {
-              saved_state.save(&fetched_item).unwrap_or_else(|err| {
-                  error!("Failed to save state: {:?}", err);
-              });
-          } else {
-              error!("Failed to fetch item: {:?}", fetch_result.err());
-          }
-      }
-  
+            info!("Matched title: {:?}", title);
+            debug!("url: {:?}", item_url);
+            let fetch_result = fetch_item(item_url, &client, &config.global_download_dir);
+            if fetch_result.is_ok() {
+                saved_state.save(&fetched_item).unwrap_or_else(|err| {
+                    error!("Failed to save state: {:?}", err);
+                });
+            } else {
+                error!("Failed to fetch item: {:?}", fetch_result.err());
+            }
+        }
     }
 }
 
@@ -164,65 +190,88 @@ fn fetch_item(
     Ok(())
 }
 
-fn setup_logger(config: &Config) -> Result<(), fern::InitError> {
-    let colors_line = ColoredLevelConfig::new()
-        .error(Color::Red)
-        .warn(Color::Yellow)
-        // depending on the terminals color scheme, this is the same as the background color
-        .trace(Color::BrightBlack);
+fn bootstrap_logger() -> Result<Handle, log4rs::Error> {
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "[{d(%Y-%m-%d %H:%M:%S)}][{h({l})}] {m}{n}",
+        )))
+        .build();
 
-    let colors_level = colors_line.info(Color::Green);
-    let mut base_config = fern::Dispatch::new();
+    let stdout_appender = Appender::builder().build("stdout", Box::new(stdout));
+    let config = log4rs::config::Config::builder()
+        .appender(stdout_appender)
+        .logger(
+            Logger::builder()
+                .appender("stdout")
+                .additive(false)
+                .build("stdout_log", LevelFilter::Trace),
+        )
+        .build(Root::builder().appender("stdout").build(LevelFilter::Trace))
+        .unwrap();
+
+    let handle = log4rs::init_config(config).unwrap();
+
+    Ok(handle)
+}
+
+fn apply_config_to_logger(handle: &Handle, config: &Config) -> Result<(), log4rs::Error> {
+    let mut config_builder = log4rs::config::Config::builder();
+    let mut root_builder = Root::builder();
+    let mut logger_builder = Logger::builder().additive(false);
+    let mut max_level = LevelFilter::Off;
+
     if let Some(log_level) = config.log_level_stdout {
-        let stdout_config = fern::Dispatch::new()
-            .format(move |out, message, record| {
-                out.finish(format_args!(
-                    "{color_line}[{date}][{level}{color_line}] {message}\x1B[0m",
-                    color_line = format_args!(
-                        "\x1B[{}m",
-                        colors_line.get_color(&record.level()).to_fg_str()
-                    ),
-                    date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                    level = colors_level.color(record.level()),
-                    message = message,
-                ));
-            })
-            // Add blanket level filter -
-            .level(log_level)
-            .level_for("tokio_reactor", log::LevelFilter::Off)
-            .level_for("reqwest", log::LevelFilter::Off)
-            .level_for("hyper", log::LevelFilter::Off)
-            .chain(std::io::stdout());
-        base_config = base_config.chain(stdout_config);
+        if log_level > max_level {
+            max_level = log_level;
+        }
+        debug!("Stdout log level: {}", log_level);
+        let stdout = ConsoleAppender::builder()
+            .encoder(Box::new(PatternEncoder::new(
+                "[{d(%Y-%m-%d %H:%M:%S)}][{h({l})}] {m}{n}",
+            )))
+            .build();
+
+        let stdout_appender = Appender::builder()
+            .filter(Box::new(ThresholdFilter::new(log_level)))
+            .build("stdout", Box::new(stdout));
+
+        config_builder = config_builder.appender(stdout_appender);
+        root_builder = root_builder.appender("stdout");
+        logger_builder = logger_builder.appender("stdout")
+    } else {
+        info!("No stdout log level found, this will be one of the last messages logged to stdout");
     }
 
     if let Some(log_path) = &config.log_file_path {
         if let Some(log_level) = config.log_level_file {
-            let file_config = fern::Dispatch::new()
-                .format(move |out, message, record| {
-                    out.finish(format_args!(
-                        "{color_line}[{date}][{level}{color_line}] {message}\x1B[0    m",
-                        color_line = format_args!(
-                            "\x1B[{}m",
-                            colors_line.get_color(&record.level()).to_fg_str()
-                        ),
-                        date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-                        level = colors_level.color(record.level()),
-                        message = message,
-                    ));
-                })
-                // Add blanket level filter -
-                .level(log_level)
-                .level_for("tokio_reactor", log::LevelFilter::Off)
-                .level_for("reqwest", log::LevelFilter::Off)
-                .level_for("hyper", log::LevelFilter::Off)
-                .chain(fern::log_file(log_path)?);
+            if log_level > max_level {
+                max_level = log_level;
+            }
+            debug!("File log level: {}", log_level);
+            let logfile = FileAppender::builder()
+                .encoder(Box::new(PatternEncoder::new(
+                    "[{d(%Y-%m-%d %H:%M:%S)}][{h({l})}] {m}{n}",
+                )))
+                .build(log_path)
+                .unwrap();
 
-            base_config = base_config.chain(file_config);
+            let file_appender = Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(log_level)))
+                .build("file", Box::new(logfile));
+
+            config_builder = config_builder.appender(file_appender);
+            root_builder = root_builder.appender("file");
+            logger_builder = logger_builder.appender("file");
         }
+    } else {
+        info!("No file log level found, won't enable file logging");
     }
 
-    base_config.apply()?;
+    config_builder = config_builder.logger(logger_builder.build("rssdownloader_rs", max_level));
+    let root = root_builder.appender("stdout").build(LevelFilter::Off);
+    let log4rs_config = config_builder.build(root).unwrap();
+
+    handle.set_config(log4rs_config);
 
     Ok(())
 }
